ae58ab873905634167655a95a01994dbdb401bbb
JorgeLuisDSP <jorgeluisds1853@gmail.com>
2025-10-23 14:29:02 -0300

    feat(public-form): adiciona validação ao form
    Este commit contém todas as validações para o Form e seus inputs,
    utilizando o Reactive Forms, junto com o FormGroup para representar o
    form, tanto o principal (publicForm) quanto o subform (problems).
    Fazendo também a utilização dos formControl para validar cada input.
    
    Também contém um CustomValidator, o 'checkboxOneChecked', que foi criado
    para fazer a validação dos inputs checkbox, verificando se pelo menos
    um deles está marcado.
    
    Utilizado getters para deixar o código do template e script mais
    limpo e de fácil manutenção.
    
    E por último, o onSubmit, para fazer o envio dos dados do
    formGroup (publicForm).

--- src/app/feactures/public-form/public-form.component.ts
+++ src/app/feactures/public-form/public-form.component.ts
@@ -1,25 +1,71 @@
-import { Component } from '@angular/core';
-import { BannerComponent } from "../../shared/banner/banner.component";
-import { RouterModule } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BannerComponent } from '../../shared/banner/banner.component';
 
 @Component({
   selector: 'app-public-form',
-  imports: [BannerComponent, RouterModule, CommonModule],
+  imports: [BannerComponent, RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './public-form.component.html',
-  styleUrl: './public-form.component.css'
+  styleUrl: './public-form.component.css',
 })
 export class PublicFormComponent {
   publicForm: FormGroup;
-  constructor(
-    private fb: FormBuilder
-  ){
+  constructor(private fb: FormBuilder) {
     this.publicForm = this.fb.group({
-      userRa: this.fb.control('', {validators: [Validators.required, Validators.pattern(/^\d{3}\.\d{6}$/)]}),
-      andarPredio: this.fb.control('', {validators: [Validators.required]}),
-      banheiro: this.fb.control('', {validators: [Validators.required]}),
-      itens: this.fb.control('',),
-    })
+      userRa: this.fb.control('', {
+        validators: [Validators.required, Validators.pattern(/^\d{3}\.\d{6}$/)],
+      }),
+      andarPredio: this.fb.control('', { validators: [Validators.required] }),
+      banheiro: this.fb.control('', { validators: [Validators.required] }),
+      problems: this.fb.group(
+        {
+          papelHigienico: false,
+          papelToalha: false,
+          sabonete: false,
+          banheiroSujo: false,
+          lixeira: false,
+          outros: false,
+        },
+        { validators: this.checboxOneChekdad }
+      ),
+    });
+  }
+
+  get userRa(): FormControl | null {
+    return this.publicForm.get('userRa') as FormControl | null;
+  }
+
+  get andarPredio(): FormControl | null {
+    return this.publicForm.get('andarPredio') as FormControl | null;
+  }
+  get banheiro():FormControl | null {
+    return this.publicForm.get('banheiro') as FormControl | null;
+  }
+
+  get problems(): FormGroup | null {
+    return this.publicForm.get('problems') as FormGroup | null;
+  }
+
+  checboxOneChekdad(group: FormGroup) {
+    const temMarcado = Object.values(group.value).some((v) => v === true);
+    return temMarcado ? null : { nenhumMarcado: true };
+  }
+
+  onSubmit() {
+    if (this.publicForm.invalid) {
+      this.publicForm.markAllAsTouched();
+      console.log('o campo esta invalido');
+      return;
+    }
+
+    console.log('Enviado com sucesso');
   }
 }
